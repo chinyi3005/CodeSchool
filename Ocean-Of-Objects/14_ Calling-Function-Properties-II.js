3d9f7fa44e784e7ecff5b0ab5de846c1c43d274c
Chinyi <chinyi3005@gmail.com>
2016-08-22 18:14:23 +0800

    complete challenge 11-14

---
+++ Ocean-Of-Objects/14_ Calling-Function-Properties-II.js
@@ -0,0 +1,11 @@
+// JavaScript Road Trip Part 3 Level 4 Challenge 14 - Calling Function Properties II
+// http://javascript-roadtrip-part3.codeschool.com/levels/4/challenges/14
+
+lighthouseRock.addBulb = function(name, wattage) {
+  this.weaponBulbs.push([name, wattage]);
+};
+
+// call addBulb and pass in the correct arguments
+lighthouseRock.addBulb("Blasterbright", 5000);
+lighthouseRock.addBulb("Sight Slayer", 1800);
+lighthouseRock.addBulb("Burner of Souls", 7500);
