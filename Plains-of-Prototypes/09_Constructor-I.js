5ec6fc11ae5fe26ffb3a2c0caf7b7a3736378e42
Chinyi <chinyi3005@gmail.com>
2016-08-29 06:26:55 +0800

    add challenge: Constructor I

---
+++ Plains-of-Prototypes/09_Constructor-I.js
@@ -0,0 +1,13 @@
+// JavaScript Road Trip Part 3 Level 5 Challenge 9
+// Constructor I
+// http://javascript-roadtrip-part3.codeschool.com/levels/5/challenges/9
+
+function Fencepost (x, y, postNum) {
+	this.x = x;
+	this.y = y;
+	this.postNum = postNum;
+	this.connectionsTo = [];
+	this.sendRopeTo = function(connectedPost) {
+		this.connectionsTo.push(connectedPost);
+	}
+}
